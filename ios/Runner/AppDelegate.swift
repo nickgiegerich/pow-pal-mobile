0db8add50e0f08eb335d87bef276d99c401d977c
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2020-12-24 17:13:26 -0700

    avy maps working!

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyB5EGdYEfgVi-yvyhocgXhOXHyDyvWv39c")
+    GMSServices.provideAPIKey("API_KEY_HERE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:collection';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pow_pal_app/api_calls/fetch_all_avalanche.dart';
@@ -11,31 +12,67 @@ class Map extends StatefulWidget {
 }
 
 class _MapPageState extends State<Map> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   Completer<GoogleMapController> _controller = Completer();
-  Future<List<AvalancheData>> avyData;
+  List<AvalancheData> avyData;
+
+  Set<Polygon> _polygons = HashSet<Polygon>();
+  // List<LatLng> polygonLatLngs = List<LatLng>();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(42.115441, -116.915637),
+    zoom: 6.5,
   );
 
   @override
   void initState() { 
     super.initState();
-    avyData = fetchAvalancheData(http.Client());
-    print(avyData);
+    fetchAvalancheData(http.Client()).then((avyDataList){
+      setState(() {
+        avyData = avyDataList;
+        for(var data in avyDataList){
+          List<LatLng> polygonLatLngs = List<LatLng>();
+          for(var point in data.coordinates){
+            LatLng newPoint = LatLng(point.lat, point.lon);
+            polygonLatLngs.add(newPoint);
+          }
+          _setPolygon(data.name, polygonLatLngs);
+        }
+        for(var polyArea in _polygons){
+          print(polyArea.points);
+        }
+      });
+    });
+  }
+
+  void _setPolygon(String areaName, List<LatLng> points) {
+    _polygons.add(Polygon(
+      polygonId: PolygonId(areaName),
+      points: points,
+      strokeWidth: 2,
+      strokeColor: Colors.blue,
+      fillColor: Color(0xff9933).withOpacity(0.5),
+    ));
   }
 
   @override
   Widget build(BuildContext context) { 
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.terrain,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+      key: _scaffoldKey,
+      body: Stack(  
+        children: <Widget>[
+          Positioned.fill(
+            child: GoogleMap( 
+              mapType: MapType.terrain,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              polygons: _polygons,
+            )
+          )
+        ],
+      )
     );
   }
 }
